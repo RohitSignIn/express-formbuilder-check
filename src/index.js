fb5c522a4a51e87647a6f6a10730315cd354accd
Rohit <rohitsinghsignin@gmail.com>
2024-05-21 12:09:40 +0530

    a

--- src/index.js
+++ src/index.js
@@ -18,9 +18,12 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", true);
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
+
+app.get('/', (req, res) => {
+  res.send("Hello")
+})
 
 app.post('/formSubmit', saveForm)
 
